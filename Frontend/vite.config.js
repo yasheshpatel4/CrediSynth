890554ff83836f3021447c61b72899326a47efcf
zeel patel <zeelp4797@gmail.com>
2025-05-06 14:16:16 +0530

    dashboard

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react({
-    jsxRuntime: 'automatic'  // Enable new JSX transform
-  })],
+    jsxRuntime: 'automatic',
+    // Enable new JSX transform
+  }), tailwindcss()],
 })
