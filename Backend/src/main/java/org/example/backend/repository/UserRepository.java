332803d8b2abf9fc0be3ff6078e0b9ff219dcde1
zeel patel <zeelp4797@gmail.com>
2025-06-08 14:58:45 +0530

    docker add

--- Backend/src/main/java/org/example/backend/repository/UserRepository.java
+++ Backend/src/main/java/org/example/backend/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package org.example.backend.repository;
+package org.example.backend.Repository;
 
 import org.example.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
